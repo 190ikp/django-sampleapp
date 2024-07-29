f692f7ee8ad83ee84183cdaa5bf00606f768cce9
190ikp <ikp190@outlook.com>
2024-07-30 03:56:42 +0900

    add some views as template

--- app/urls.py
+++ app/urls.py
@@ -1,1 +1,11 @@
 from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', EmployeeIndexView.as_view(), name='index'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('detail/<int:pk>/', EmployeeDetailView.as_view(), name='detail'),
+    path('register/', EmployeeCreateView.as_view(), name='register'),
+    path('update/<int:pk>/', EmployeeUpdateView.as_view(), name='update'),
+    path('create/department', DepartmentCreateView.as_view(), name='create_department'),
+]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.views.generic import *
 
 # Create your views here.
+
+class LoginView():
+    pass
+
+# if not logged in, redirect to login page
+class EmployeeIndexView(ListView):
+    pass
+
+class EmployeeDetailView(DetailView):
+    pass
+
+class EmployeeCreateView(CreateView):
+    pass
+
+class EmployeeUpdateView(UpdateView):
+    pass
+
+class DepartmentCreateView(CreateView):
+    pass
+
